d9837c98061a289ca8d65b801b7cd08dfd0cc3dc
Martin Kolomazník <martin.kolomaznik@etnetera.cz>
2022-12-04 14:57:08 +0100

    Initial Commit

---
+++ Weather-TCA/Weather_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TCAApp.swift
+//  Weather-TCA
+//
+//  Created by martin.kolomaznik on 04.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
